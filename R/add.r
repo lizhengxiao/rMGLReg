9d3a139e396e9a79c73139959233d4d6483e5106
Lee <41932717+lizhengxiao@users.noreply.github.com>
2021-07-29 16:57:46 +0800

    1
    1

---
+++ R/add.r
@@ -0,0 +1,15 @@
+# 让我们完成编写
+#' Returns the sum of two numbers
+#'
+#' @param a Number one.
+#' @param b Number two.
+#'
+#' @return Sum of two number.
+#' @export
+#'
+#' @examples
+#' library(rPackageTutorial)
+#' add(1, 2)
+add<-function(a,b) {
+  a+b
+}
