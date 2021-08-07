3ba5c17e3abe8cf0f04a22c84ea070d72066fd54
lizhengxiao <li_zhengxiao@hotmail.com>
2021-08-07 16:41:50 +0800

    2021-08-07

--- R/MGL-mle-mixed.r
+++ R/MGL-mle-mixed.r
@@ -6,16 +6,20 @@
 #' @param hessian Logical. Should a numerically differentiated Hessian matrix be returned?
 #' @param initpar Initial values for the parameters to be optimized over.
 #' @param ... additional arguments to be passed to f.
+#' @param U_ asd
+#' @param obs two-dimenstional matrix for observations
+#' @param f values of the density function for marginal distribution
+#' @param umin sd
 #' @importFrom stats nlm
 #' @return A list containing the following components
 #' @export
 #'
-MGL.mle <- function(obs, U, U_, f, copula = c(
+MGL.mle.mixed <- function(obs, U, U_, f, copula = c(
   "MGL", "MGL180", "MGL-EV",
   "MGL-EV180",
   "Gumbel",
   "Normal", "MGB2", "t"
-),
+), umin,
 hessian = TRUE,
 initpar, ...) {
   dnormcop <- function(U, param) {
@@ -60,17 +64,30 @@ initpar, ...) {
     VineCopula::BiCopHfunc(U[,1], U[,2], family = 2, par = param[1], par2 = param[2])
   hgumcop <- function(U, param)
     VineCopula::BiCopHfunc(U[,1], U[,2], family = 4, par = param[1])
-
-
-
+  hMGL <- function(U, param){
+    hcMGL.bivar(u1 = U[,1], u2 = U[,2], pars = param[1])}
+  hMGL180 <- function(U, param){
+    hfunc1 <- 1 - hcMGL.bivar(u1 = 1 - U[,1], u2 = 1 - U[,2], pars = param[1])$hfunc1
+    hfunc2 <- 1 - hcMGL.bivar(u1 = 1 - U[,1], u2 = 1 - U[,2], pars = param[1])$hfunc2
+    out <- list(hfunc1 = hfunc1, hfunc2 = hfunc2)
+    out}
+  hMGLEV180 <- function(U, param){
+    hfunc1 <- hMGLEV180.bivar(u1 = U[,1], u2 = U[,2], param = param[1])$hfunc1
+    hfunc2 <- hMGLEV180.bivar(u1 = U[,1], u2 = U[,2], param = param[1])$hfunc2
+    out <- list(hfunc1 = hfunc1, hfunc2 = hfunc2)
+    out}
+  hMGLEV <- function(U, param){
+    hfunc1 <- 1 - hMGLEV180.bivar(u1 = 1 - U[,1], u2 = 1 - U[,2], param = param[1])$hfunc1
+    hfunc2 <- 1 - hMGLEV180.bivar(u1 = 1 - U[,1], u2 = 1 - U[,2], param = param[1])$hfunc2
+    out <- list(hfunc1 = hfunc1, hfunc2 = hfunc2)
+    out}
+  hMGB2 <- function(U, param){
+    hcMGB2.bivar(u1 = U[,1], u2 = U[,2], pars1 = param[1], pars2 = param[2], pars3 = param[3])}
 
   argnorm <- list(length = 1, lower = 0, upper = 1, name = "Gaussian")
   argt <- list(length = 2, lower = c(0, 0), upper = c(1, 100), name = "Student")
   arggum <- list(length = 1, lower = 1, upper = 50, name = "Gumbel")
-  argMGB2 <- list(
-    length = 3, lower = c(0, 0, 0), upper = c(50, 50, 50),
-    name = "MGB2"
-  )
+  argMGB2 <- list(length = 3, lower = c(0, 0, 0), upper = c(50, 50, 50), name = "MGB2")
   argMG180 <- list(length = 1, lower = 0, upper = 10, name = "MGL180")
   argMGLEV180 <- list(length = 1, lower = 0, upper = 10, name = "MGL-EV180")
   argMG <- list(length = 1, lower = 0, upper = 10, name = "MGL")
@@ -78,43 +95,54 @@ initpar, ...) {
   if (copula == "MGL") {
     dcop <- dMGL
     arg.cop <- argMG
+    hcop <-   hMGL
   } else if (copula == "MGL180") {
     dcop <- dMGL180
     arg.cop <- argMG180
+    hcop <- hMGL180
   } else if (copula == "MGL-EV") {
     dcop <- dMGLEV
     arg.cop <- argMGLEV
+    hcop <- hMGLEV
   } else if (copula == "MGL-EV180") {
     dcop <- dMGLEV180
     arg.cop <- argMGLEV180
+    hcop <- hMGLEV180
   } else if (copula == "Gumbel") {
     dcop <- dgumcop
     arg.cop <- arggum
+    hcop <-   hgumcop
   } else if (copula == "Normal") {
     dcop <- dnormcop
     arg.cop <- argnorm
+    hcop <- hnormcop
   } else if (copula == "t") {
     dcop <- dtcop
     arg.cop <- argt
+    hcop <-    htcop
   } else if (copula == "MGB2") {
     dcop <- dMGB2
     arg.cop <- argMGB2
+    hcop <-   hMGB2
   }
 
   # loglike.copula <- function(U, initpar, ...){
+  Obs1 <- obs[,1] # y1
+  Obs2 <- obs[,2] # y2
+  f1 <- f[,1]
+  f2 <- f[,2]
   copLogL <- function(x) {
-    if (all(arg.cop$lower < x && arg.cop$upper > x)) {
-      logL <- log(dcop(U, param = x))
-      # res <- -sum((logL))
-      remove.naninf <- function(x) x[!is.nan(x) & is.finite(x)]
-      res <- -sum(remove.naninf(logL))
-    } else {
-      res <- 10000000000000
-    }
-    return(res)
-  }
-  # }
-
+    if(all(arg.cop$lower < x && arg.cop$upper > x)){
+      index1 <- which(Obs2 <= umin)
+      index2 <- which(Obs2 > umin)
+      m1 <- f1[index1]*(hcop(U[index1,], param = x)$hfunc1 - hcop(U_[index1,], param = x)$hfunc1)
+      logL1 <- (log(m1))
+      m2 <- f1[index2]*f2[index2]*dcop(U[index2,], param = x)
+      logL2 <- (log(m2))
+      ll <- c(logL1, logL2)
+      res <- -sum((ll)) # 定义似然函数
+    } else { res <- 100000000000000000 }
+    return(res)}
   resopt <- nlm(
     f = copLogL,
     # U = U,
--- R/MGLEV-bivariate.r
+++ R/MGLEV-bivariate.r
@@ -181,3 +181,107 @@ dcMGLEV180.bivar <- function(u1, u2, param) {
   out <- p1 * p2
   return(out)
 }
+
+
+
+#' @rdname  BSMGL-EV
+#' @export
+#' @examples
+#' hMGLEV180.bivar(u1 = 0.5, u2 = 0.78, param = 1.8)
+hMGLEV180.bivar <- function(u1, u2, param){
+  delta <- param
+  lf <- function(y1, y2, param){
+    delta <- param
+    z2 <- (y2)^(- delta)/(y1^(- delta) + y2^(- delta))
+    z1 <- (y1)^(- delta)/(y1^(- delta) + y2^(- delta))
+    out <- y1*pbeta(z2, shape1 = 0.5, shape2 = 1/delta + 0.5) + y2*pbeta(z1, shape1 = 0.5, shape2 = 1/delta + 0.5)
+    return(out)
+  }
+
+  pbeta_dev <- function(y, shape1, shape2){
+    (1 - y)^(shape2 - 1)*y^(shape1 - 1)/beta(shape1, shape2)
+  }
+
+  pbeta_dev2 <- function(y, shape1, shape2){
+    -(1 - y)^(shape2-2)*y^(shape1-2)*((y-1)*shape1 + (shape2-2)*y+1)/beta(shape1, shape2)
+  }
+  z2_x1 <- function(x1, x2, param){
+    delta <- param
+    part1 <- delta*x1^(-delta-1)*x2^(-delta)
+    part2 <- (x2^(-delta) + x1^(-delta))^2
+    part1/part2
+  }
+
+  z1_x1 <- function(x1, x2, param){
+    delta <- param
+    part1 <- -delta*x1^(-delta-1)*x2^(-delta)
+    part2 <- (x2^(-delta) + x1^(-delta))^2
+    part1/part2
+  }
+
+  z2_x2 <- function(x1, x2, param){
+    delta <- param
+    part1 <- -delta*x2^(-delta-1)*x1^(-delta)
+    part2 <- (x2^(-delta) + x1^(-delta))^2
+    part1/part2
+  }
+
+  z1_x2 <- function(x1, x2, param){
+    delta <- param
+    part1 <- delta*x2^(-delta-1)*x1^(-delta)
+    part2 <- (x2^(-delta) + x1^(-delta))^2
+    part1/part2
+  }
+
+  z2_x1x2 <- function(x1, x2, param){
+    delta <- param
+    part1 <- delta^2*(x1*x2)^(-delta-1)*(x2^(-delta) - x1^(-delta))
+    part2 <- (x2^(-delta) + x1^(-delta))^3
+    part1/part2
+  }
+
+  z1_x1x2 <- function(x1, x2, param){
+    delta <- param
+    part1 <- delta^2*(x1*x2)^(-delta-1)*(x1^(-delta) - x2^(-delta))
+    part2 <- (x2^(-delta) + x1^(-delta))^3
+    part1/part2
+  }
+
+
+
+  lf_dev_x1 <- function(x1, x2, param){
+    delta <- param
+    z2 <- x2^(-delta)/(x2^(-delta) + x1^(-delta))
+    z1 <- 1 - z2
+    out <- pbeta(z2, shape1 = 0.5, shape2 = 1/delta + 0.5) + x1*pbeta_dev(z2, shape1 = 0.5, shape2 = 1/delta + 0.5)*z2_x1(x1 = x1, x2 = x2, param = delta) + x2*pbeta_dev(z1, shape1 = 0.5, shape2 = 1/delta + 0.5)*z1_x1(x1 = x1, x2 = x2, param = delta)
+    out
+  }
+
+
+  lf_dev_x2 <- function(x1, x2, param){
+    delta <- param
+    z2 <- x2^(-delta)/(x2^(-delta) + x1^(-delta))
+    z1 <- 1 - z2
+    out <- pbeta(z1, shape1 = 0.5, shape2 = 1/delta + 0.5) + x2*pbeta_dev(z1, shape1 = 0.5, shape2 = 1/delta + 0.5)*z1_x2(x1 = x1, x2 = x2, param = delta) + x1*pbeta_dev(z2, shape1 = 0.5, shape2 = 1/delta + 0.5)*z2_x2(x1 = x1, x2 = x2, param = delta)
+    out
+  }
+
+  # final results
+  Cu1u2 <- pcMGLEV180.bivar(u1 = u1, u2 = u2, param = delta)
+  index1 <- u1 == 0 & u2 != 0
+  index2 <- u1 != 0& u2 == 0
+  index3 <- u1 == 0 & u2 == 0
+  hfunc1 <- Cu1u2/u1*lf_dev_x1(x1 = -log(u1), x2 = -log(u2), param = delta)
+  hfunc2 <- Cu1u2/u2*lf_dev_x2(x1 = -log(u1), x2 = -log(u2), param = delta)
+
+  # utemp <- cbind(u1, u2)
+  u10 <- rep(1e-10, length(u1[index1]))
+  u20 <- rep(1e-10, length(u2[index2]))
+  hfunc1[index1] <- pcMGLEV180.bivar(u1 = u10, u2 = u2[index1], param = delta)/u10*lf_dev_x1(x1 = -log(u10), x2 = -log(u2[index1]), param = delta)
+  hfunc2[index1] = 0.0000000001
+
+  hfunc1[index2] = 0.0000000001
+  hfunc2[index2] <- pcMGLEV180.bivar(u1 = u1[index2], u2 = u20, param = delta)/u20*lf_dev_x2(x1 = -log(u1[index2]), x2 = -log(u20), param = delta)
+
+  list(hfunc1 = hfunc1, hfunc2 = hfunc2) # h_1(u_2|u_1,??)
+}
